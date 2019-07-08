04f4aa44e4aeed6e610b12c3c449ce233747e47f
Aaron Jacobs <aaron.jacobs@crescendotechnology.com>
2019-07-08 22:03:26 +0000

    Adds general-purpose CSV and JSON layouts.

--- tests/testthat/test-layouts.R
+++ tests/testthat/test-layouts.R
@@ -6,6 +6,20 @@ test_that("Basic layouts work correctly", {
 
   layout <- default_log_layout()
   expect_match(layout("INFO", "Message"), "Message")
+
+  layout <- csv_log_layout()
+  expect_match(layout("INFO", "Message"), ",Message\\n", )
+  expect_match(layout("INFO", "Message", "Second"), ",Message,Second\\n")
+  layout <- csv_log_layout(delimiter = "\t")
+  expect_match(layout("INFO", "Message"), "\\tMessage\\n")
+})
+
+test_that("JSON layouts work correctly", {
+  skip_if_not_installed("jsonlite")
+
+  layout <- json_log_layout()
+  expect_match(layout("INFO", "Message"), "\"message\":\"Message\"")
+  expect_match(layout("INFO", field = "value"), "\"field\":\"value\"")
 })
 
 test_that("Wonky times formats are caught early", {
